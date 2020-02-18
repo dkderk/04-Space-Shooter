4792fce722a7b1dd71a2918967eec640b38a1596
Derek <derekkarmolinski@gmail.com>
2020-02-17 23:42:52 -0500

    Checked if var name was the problem

--- Scripts/Enemies.gd
+++ Scripts/Enemies.gd
@@ -3,8 +3,8 @@ extends Node2D
 export var max_enemies = 5
 export var probability = 0.3
 
-onready var Ufo1 = load("res://Scenes/ufo1.tscn")
-onready var Ufo2 = load("res://Scenes/ufo2.tscn")
+onready var Enemy1 = load("res://Scenes/ufo1.tscn")
+onready var Enemy2 = load("res://Scenes/ufo2.tscn")
 
 func _ready():
 	randomize()
@@ -14,8 +14,8 @@ func _on_Timer_timeout():
 	if get_child_count() < max_enemies + 1:
 		if randf() < probability or get_child_count() == 1:
 			if randf() < 0.5:
-				var e = Ufo1.instance()
+				var e = Enemy1.instance()
 				add_child(e)
 			else:
-				var e = Ufo2.instance()
+				var e = Enemy2.instance()
 				add_child(e)
